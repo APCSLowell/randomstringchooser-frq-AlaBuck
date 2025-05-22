3269ec843835e9af47dc93db6f074713eff5faec
AlaBuck <Albuckingham@s.sfusd.edu>
2025-05-22 11:28:14 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -2,7 +2,24 @@ import java.util.*;
 public class RandomStringChooser
 {
   /* to be implemented in part (a) */
-  
-  //Heads up! 
-  //You will get a very confusing error message until you have working code in part b as well
-}
+  private String[] words;
+  private int numsleft;
+  public RandomStringChooser(String[] wordArray){
+    words = new String[wordArray.length];
+
+    for(int i=0; i<wordArray.length; i++){
+      words[i] = wordArray[i];
+    }
+    numsleft = words.length;
+  }
+  public String getNext() {
+    if(numsleft==0){
+      return "NONE";
+    }
+    int index= (int)(Math.random()*numsleft);
+    String selected = words[index];
+    words[index] = words[numsleft-1];
+    numsleft--;
+    return selected;
+    }
+  }
